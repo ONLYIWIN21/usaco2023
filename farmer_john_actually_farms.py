edd83d7050a9216a0348a79cd9a6cba7d20fb0fa
ONLYIWIN21 <flintthecuber@gmail.com>
2023-12-16 21:45:34 -0500

    finish

--- farmer_john_actually_farms.py
+++ farmer_john_actually_farms.py
@@ -1,8 +1,8 @@
-def is_sorted(lst):
+def is_sorted(lst, key):
     for i in range(len(lst) - 1):
-        if lst[i][1] <= lst[i + 1][1]:
-            return False
-    return True
+        if key(lst[i]) <= key(lst[i + 1]):
+            return i
+    return -1
 
 num_tests = int(input())
 
@@ -20,16 +20,29 @@ for i in range(num_tests):
 
     plants.sort(key=lambda plant: plant[0])
 
+    max = 100000000
+    plants_sorted = is_sorted(plants, lambda plant: plant[2])
+    if plants_sorted != -1:
+        if plants[plants_sorted][2] == plants[plants_sorted + 1][2]:
+            if plants[plants_sorted][1] <= plants[plants_sorted + 1][1]:
+                max = 0
+        else:
+            max = (plants[plants_sorted][1] - plants[plants_sorted + 1][1]) // (plants[plants_sorted + 1][2] - plants[plants_sorted][2]) + 1
+
     min = -1
-    for i in range(10000):
-        if is_sorted(plants):
+    i = 0
+    while i < max:
+        plants_sorted = is_sorted(plants, lambda plant: plant[1] + plant[2] * i)
+        if plants_sorted == -1:
             min = i
             break
-        for j in range(len(plants)):
-            plants[j][1] += plants[j][2]
+        new_i = (plants[plants_sorted][1] - plants[plants_sorted + 1][1]) // (plants[plants_sorted + 1][2] - plants[plants_sorted][2]) + 1
+        if new_i <= i:
+            break
+        i = new_i
 
     output += str(min)
 
     output += "\n"
 
-print(output)
+print(output[:-1])
