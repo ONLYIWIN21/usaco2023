0b97af65d4ff68c6fba02e52788adaa5413eb4fd
ONLYIWIN21 <flintthecuber@gmail.com>
2023-12-16 19:37:36 -0500

    all but 1

--- cowntact_tracing.py
+++ cowntact_tracing.py
@@ -15,15 +15,19 @@ for cow in input():
 if curr_len > 0:
     blocks.append(curr_len)
 
-min_time = 99999
-for block in blocks:
+max_time = 99999999
+for i in range(len(blocks)):
+    block = blocks[i]
     block_time = block // 2 + (block % 2) - 1
-    min_time = min(block_time, min_time)
+    if i == 0 or i == len(blocks) - 1:
+        block_time = block - 1
+    max_time = min(block_time, max_time)
 
-#print("min time: " + str(min_time))
+#print("blocks: " + str(blocks))
+#print("max time: " + str(max_time))
 num_cows = 0
 for block in blocks:
-    cows = math.ceil(block / (min_time * 2 + 1))
+    cows = math.ceil(block / (max_time * 2 + 1))
     #print(cows)
     num_cows += cows
 
